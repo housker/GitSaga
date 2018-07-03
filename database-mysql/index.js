54a744e78ad1c484d5ee991ba0846f4cc3c57c74
Adelle Housker <housker@gmail.com>
2018-07-03 09:09:38 -0500

    adding or statement to connect to local database

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database : process.env.RDS_DB_NAME
+  host     : process.env.RDS_HOSTNAME || 'localhost',
+  user     : process.env.RDS_USERNAME || 'root',
+  password : process.env.RDS_PASSWORD || '',
+  port     : process.env.RDS_PORT || '',
+  database : process.env.RDS_DB_NAME || 'gitsaga'
 });
 
 // var connection = mysql.createConnection({
@@ -25,8 +25,10 @@ var check = function() {
 var selectChapter = function(title, callback) {
   connection.query(`SELECT * FROM chapters WHERE title = '${title}' ORDER BY updated DESC LIMIT 1`, function(err, results, fields) {
     if(err) {
+      console.log('error in selectChapter: ', err)
       callback(err, null);
     } else {
+      console.log('results from selectChapter: ', results)
       callback(null, results);
     }
   });
@@ -35,10 +37,8 @@ var selectChapter = function(title, callback) {
 var selectCities = function(callback) {
   connection.query('SELECT title, geolocation FROM chapters GROUP BY title', function(err, results, fields) {
     if(err) {
-      console.log('error: ', err)
       callback(err, null);
     } else {
-      console.log('results in db: ', results)
       callback(null, results);
     }
   });
@@ -83,8 +83,10 @@ var insert = function(body, callback) {
   var values = [body.title, body.content, body.geolocation, body.votes]
   connection.query('INSERT INTO chapters (title, content, geolocation, votes) VALUES(?, ?, ?, ?)', values, function(err, results, fields) {
     if(err) {
+      console.log('error in insert: ', err)
       callback(err, null);
     } else {
+      console.log('results in insert: ', results)
       callback(null, results);
     }
   })
